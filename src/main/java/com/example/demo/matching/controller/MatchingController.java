0411118082441afae77940ce49e6e9576819f338
dlwldn30 <dlwldn30@naver.com>
2025-12-23 21:44:09 +0900

    fdsnkljflads

--- src/main/java/com/example/demo/matching/controller/MatchingController.java
+++ src/main/java/com/example/demo/matching/controller/MatchingController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/matching")
+@RequestMapping("/matching")
 public class MatchingController {
 
     private final MatchingService matchingService;
